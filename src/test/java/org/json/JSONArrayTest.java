ff9cf8efd0f9a95b1636ff9b9d4f5170c243db84
Kerby Martino <kerbymart@gmail.com>
2025-07-22 12:05:56 +0800

    test: Add tests for JSONArray primitive and wrapper types

--- src/test/java/org/json/JSONArrayTest.java
+++ src/test/java/org/json/JSONArrayTest.java
@@ -16,6 +16,7 @@
 package org.json;
 
 import com.google.gwt.json.client.JSONException;
+import com.google.gwt.json.client.JSONNull;
 import com.google.gwt.junit.client.GWTTestCase;
 
 import java.math.BigDecimal;
@@ -96,5 +97,90 @@ public class JSONArrayTest extends GWTTestCase {
         jsonArray.put(2.5f);
         // This may not give exact 2.5f due to bug using longValue()
         assertEquals(2.0f, jsonArray.getFloat(0), 0.001f);
+    }
+
+    public void testPutAndGetPrimitives() throws JSONException {
+        jsonArray.put(true).put(false);
+        jsonArray.put(1.234d).put(2.5f);
+        jsonArray.put(42).put(123456789L);
+
+        assertEquals(6, jsonArray.length());
+        assertTrue(jsonArray.getBoolean(0));
+        assertFalse(jsonArray.getBoolean(1));
+        assertEquals(1.234d, jsonArray.getDouble(2));
+        assertEquals(2.5f, jsonArray.getFloat(3), 0.0001f);
+        assertEquals(42, jsonArray.getInt(4));
+        assertEquals(123456789L, jsonArray.getLong(5));
+    }
+
+    public void testPutAndGetWrappersAndNulls() throws JSONException {
+        jsonArray.put((Boolean) null)
+                .put((Double) null)
+                .put((Float) null)
+                .put((Integer) null)
+                .put((Long) null)
+                .put((JSONArray) null);
+
+        assertEquals(6, jsonArray.length());
+        assertNull(jsonArray.get(0));
+        assertNull(jsonArray.getBigDecimal(1));
+        assertNull(jsonArray.getFloat(2));
+        assertNull(jsonArray.getInt(3));
+        assertNull(jsonArray.getLong(4));
+        assertNull(jsonArray.getJSONArray(5));
+    }
+
+    public void testGetBigDecimalAndBigInteger() throws JSONException {
+        jsonArray.put(10.75).put(123456789);
+        BigDecimal bd = jsonArray.getBigDecimal(0);
+        BigInteger bi = jsonArray.getBigInteger(1);
+
+        assertEquals(new BigDecimal("10.75"), bd);
+        assertEquals(BigInteger.valueOf(123456789L), bi);
+    }
+
+    public void testGetNumberAndStringAndNull() throws JSONException {
+        jsonArray.put("hello");
+        jsonArray.put(3.14);
+        jsonArray.put((String) null);
+
+        assertEquals("hello", jsonArray.getString(0));
+        assertEquals(Double.valueOf(3.14), jsonArray.getNumber(1));
+        assertNull(jsonArray.getString(2));
+    }
+
+    public void testNestedJSONArrayAndJSONObject() throws JSONException {
+        JSONArray innerArray = new JSONArray();
+        innerArray.put(1).put(2);
+        JSONObject innerObj = new JSONObject();
+        innerObj.put("key", "value");
+
+        jsonArray.put(innerArray).put(innerObj);
+
+        JSONArray outArr = jsonArray.getJSONArray(0);
+        JSONObject outObj = jsonArray.getJSONObject(1);
+
+        assertNotNull(outArr);
+        assertEquals(2, outArr.length());
+        assertEquals(1, outArr.getInt(0));
+
+        assertNotNull(outObj);
+        assertEquals("value", outObj.getString("key"));
+    }
+
+    public void testUnsupportedObjectThrows() throws JSONException {
+        try {
+            jsonArray.put((Object) new java.util.Date());
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+
+    public void testIsNullAndIsEmpty() throws JSONException {
+        assertTrue(jsonArray.isEmpty());
+        jsonArray.put(JSONNull.getInstance());
+        assertFalse(jsonArray.isEmpty());
+        assertTrue(jsonArray.isNull(0));
     }
 }
